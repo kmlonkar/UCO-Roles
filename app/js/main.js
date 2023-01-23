d6181778a67d9270c84b718f931c2940347080f4
Komal Lonkar <komal.lonkar@neosoftmail.com>
2023-01-23 19:14:04 +0530

    design changes and responsive

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,18 @@
 $(function(){
   $('#datepicker1').datepicker();
+  $('#datepicker2').datepicker();
   $('.date').datepicker();
   $('#date-2').datepicker({
     format: "dd-M-yy"
   });
+  $('.search-input').focus(function () {
+    $('.search-options').addClass('search-options--open');
+    $('.search-results').addClass('search-results--open');
+    
+  })
+   $('.search-input').blur(function () {
+     $('.search-options').removeClass('search-options--open');
+     $('.search-results').removeClass('search-results--open');
+
+   })
 });
